25ee1ab31e6aece85ad4783cf2c952faba737e0f
felipereblin <felipereblin7@gmail.com>
2018-10-14 16:16:47 -0300

    criação de dao e main

--- src/banco/Principal.java
+++ src/banco/Principal.java
@@ -2,136 +2,48 @@ package banco;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import banco.dao.DbConnection;
 import banco.modelo.Cliente;
 import banco.modelo.Conta;
+import banco.modelo.Pessoa;
 
 
 public class Principal {
 
 	public static void main(String[] args) {
-		ArrayList<Conta> banco = new ArrayList<>();
 
-		System.out.println("Agência Bancária");
-			
-		//contasComFlag(banco);
+		DbConnection pdb = new DbConnection();
 		
-		contasComSentinela(banco);
+		Cliente c1 = new Cliente(0, "Jose", "Rua Loka", 222222, 00000, 555555, 15000);
+		Cliente c2 = new Cliente(0, "Jose", "Rua Loka", 111111, 77777, 888888, 17000);
 		
-		for (Conta c : banco) {
-			System.out.println("Conta corrente: ");
-			System.out.println(c);
-		}
-
-	}
-	
-	@SuppressWarnings("unused")
-	private static void contasComFlag(ArrayList<Conta> banco) {
-		boolean continua = true;
+		pdb.addCliente(c1);
+		pdb.addCliente(c2);
 		
-		Scanner entrada = new Scanner(System.in);
-		
-		while (continua) {		
-			Cliente cliente = new Cliente();
-			Conta conta;
-			
-			System.out.print("Entre o nome do cliente: ");
-			cliente.setNome(entrada.nextLine());
-			
-			System.out.print("Entre o cpf do cliente: ");
-			cliente.setCpf(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre o rg do cliente: ");
-			cliente.setRg(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre o endereço do cliente: ");
-			cliente.setEndereco(entrada.nextLine());
-			
-			System.out.print("Entre o telefone do cliente: ");
-			cliente.setTelefone(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre a renda mensal do cliente: ");
-			cliente.setRendaMensal(entrada.nextDouble());
-			entrada.nextLine();
-			
-			System.out.print("Entre a agência da conta bancária: ");
-			int agencia = entrada.nextInt();
-			entrada.nextLine();
-			
-			System.out.print("Entre o número da conta bancária: ");
-			int numero = entrada.nextInt();
-			entrada.nextLine();
-			
-			System.out.print("Entre o saldo inicial: ");
-			double saldo = entrada.nextDouble();
-			entrada.nextLine();
-						
-			conta = new Conta(agencia, numero, cliente, saldo);
-			
-			banco.add(conta);
-			
-			System.out.print("Deseja continuar? (s/n) ");
-			continua = entrada.nextLine().equals("s");
-
+		System.out.println("Base Inicial: ");
+		for (Cliente cliente : pdb.getClientes()) {
+			System.out.println(cliente);
+			System.out.println("----------------------------------");
 		}
 		
-		entrada.close();
-	}
-	
-	private static void contasComSentinela(ArrayList<Conta> banco) {
-		Scanner entrada = new Scanner(System.in);
+		c1.setEndereco("Rua legal");
+		c2.setNome("Felipe");
 		
-		Cliente cliente = new Cliente();
+		pdb.updateCliente(c1);
+		pdb.updateCliente(c2);
 		
-		System.out.print("Entre o nome do cliente, ou deixe em branco para terminar: ");
-		cliente.setNome(entrada.nextLine());
-		
-		while (!cliente.getNome().isEmpty()) {		
-			Conta conta;
-						
-			System.out.print("Entre o cpf do cliente: ");
-			cliente.setCpf(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre o rg do cliente: ");
-			cliente.setRg(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre o endereço do cliente: ");
-			cliente.setEndereco(entrada.nextLine());
-			
-			System.out.print("Entre o telefone do cliente: ");
-			cliente.setTelefone(entrada.nextLong());
-			entrada.nextLine();
-			
-			System.out.print("Entre a renda mensal do cliente: ");
-			cliente.setRendaMensal(entrada.nextDouble());
-			entrada.nextLine();
-			
-			System.out.print("Entre a agência da conta bancária: ");
-			int agencia = entrada.nextInt();
-			entrada.nextLine();
-			
-			System.out.print("Entre o número da conta bancária: ");
-			int numero = entrada.nextInt();
-			entrada.nextLine();
-			
-			System.out.print("Entre o saldo inicial: ");
-			double saldo = entrada.nextDouble();
-			entrada.nextLine();
-						
-			conta = new Conta(agencia, numero, cliente, saldo);
-			
-			banco.add(conta);
-			cliente = new Cliente();
-			
-			System.out.print("Entre o nome do cliente, ou deixe em branco para terminar: ");
-			cliente.setNome(entrada.nextLine());
+		System.out.println("Pós Update: ");
+		for (Cliente cliente : pdb.getClientes()) {
+			System.out.println(cliente);
+			System.out.println("----------------------------------");
 		}
 		
-		entrada.close();
-
+		pdb.deleteCliente(1);
+		
+		System.out.println("Pós delete: ");
+		for (Cliente cliente : pdb.getClientes()) {
+			System.out.println(cliente);
+			System.out.println("----------------------------------");
+		}
 	}
 }
--- src/banco/modelo/Cliente.java
+++ src/banco/modelo/Cliente.java
@@ -2,15 +2,24 @@ package banco.modelo;
 
 
 public class Cliente extends Pessoa {
+	private int clienteID;
 	private double rendaMensal;
 
 	public Cliente() { super(); }
 	
-	public Cliente(String nome, String endereco, long cpf, long rg,
+	public Cliente(int clienteID, String nome, String endereco, long cpf, long rg,
 			long telefone, double rendaMensal) {
 		super(nome, endereco, cpf, rg, telefone);
 
 		this.rendaMensal = rendaMensal;
+	}
+
+	public int getClienteID() {
+		return clienteID;
+	}
+
+	public void setClienteID(int clienteID) {
+		this.clienteID = clienteID;
 	}
 
 	public double getRendaMensal() {
